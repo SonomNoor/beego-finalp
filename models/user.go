4cda356089ba2fb61712565b08bd8e1a886e8472
Sonom Rubayat Noor <srnoor95@gmail.com>
2021-12-17 15:37:15 +0600

    validation checker added

--- models/user.go
+++ models/user.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"regexp"
 
 	_ "github.com/lib/pq"
 	// "strconv"
@@ -14,20 +15,25 @@ var (
 	UserList map[string]*User
 )
 
-func init() {
+/* func init() {
 	UserList = make(map[string]*User)
 	u := User{1, "Sonom", "Sonom", "srnoor95@gmail.com", "01677734142", "1234", "29-04-1998"}
 	UserList["user_11111"] = &u
+} */
+
+func init() {
+	Objects = make(map[string]*Object)
+
 }
 
 type User struct {
-	Id        int64
-	Firstname string
-	Lastname  string
-	Email     string
-	Phone     string
-	Password  string
-	DoB       string
+	Id          int64
+	Firstname   string
+	Lastname    string
+	Email       string
+	Phone       string
+	Password    string
+	DateOfBirth string
 }
 
 func AddUser(u User) string {
@@ -48,19 +54,31 @@ func AddUser(u User) string {
 
 	// close database
 	defer db.Close()
+	//validationofEmail
+	var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	email := emailRegexp.MatchString(u.Email)
+	fmt.Println(email)
+
+	//validationOfPhone
+	var phoneRegx = regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+	phone := phoneRegx.MatchString(u.Phone)
+	fmt.Println(phone)
+	//validationOfdateOfBirth
+	var dobRegx = regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+	dob := dobRegx.MatchString(u.DateOfBirth)
+	fmt.Println(dob)
 
 	sql := `INSERT INTO "user_table"("id","first_name", "last_name", "email", "phone", "password", "dob") VALUES ($1, $2, $3, $4, $5, $6,$7)`
-	_, e := db.Exec(sql, u.Id, u.Firstname, u.Lastname, u.Email, u.Phone, u.Password, u.DoB)
+	_, e := db.Exec(sql, u.Id, u.Firstname, u.Lastname, u.Email, u.Phone, u.Password, u.DateOfBirth)
 	CheckError(e)
 	// check db
 	err = db.Ping()
 	CheckError(err)
 
 	fmt.Println("Connected!")
+	fmt.Println(u.Email)
 
-	// u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	// UserList[u.Id] = &u
-	return u.Firstname
+	return u.Email
 }
 
 func CheckError(err error) {
